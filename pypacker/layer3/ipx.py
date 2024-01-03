85fa49fa9cc3d0c1274ce9448495cf1ffc864ee9
Michael Stahn <michael.stahn.42@gmail.com>
2024-01-03 20:11:19 +0100

    Hints for IPX documentation

--- pypacker/layer3/ipx.py
+++ pypacker/layer3/ipx.py
@@ -1,7 +1,12 @@
 # Copyright 2013, Michael Stahn
 # Use of this source code is governed by a GPLv2-style license that can be
 # found in the LICENSE file.
-"""Internetwork Packet Exchange."""
+"""
+Internetwork Packet Exchange (proprietary protocol)
+
+https://www.novell.com/documentation/nw6p/?page=/documentation/nw6p/ipx_enu/data/hc1w6pvi.html
+https://de.wikipedia.org/wiki/Internetwork_Packet_Exchange
+"""
 
 from pypacker import pypacker
 
